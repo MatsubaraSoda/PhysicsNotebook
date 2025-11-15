ac87ba5522944497d9264c0db2760173463cfd3d
MatsubaraSoda <MatsubaraSoda@gmail.com>
2025-11-15 20:36:42 +0800

    更新 电路基础 频率响应（未完成）

--- docs/.vitepress/theme/components/Img.vue
+++ docs/.vitepress/theme/components/Img.vue
@@ -10,8 +10,9 @@ import { defineProps } from 'vue';
 const props = defineProps({
     src: String,
     alt: String,
-    maxHeight: { type: String, default: '350px' }
+    maxHeight: { type: String, default: '350px' },
+    width: { type: String, default: 'auto' }
 })
 
-const style = `max-height: ${props.maxHeight}; background-color: white; padding: 10px; border-radius: 5px;`
+const style = `max-height: ${props.maxHeight}; width: ${props.width}; background-color: white; padding: 10px; border-radius: 5px;`
 </script>
